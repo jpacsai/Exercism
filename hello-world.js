f3b628ff971f60909afb00790936e5535ec24d2f
unknown <judit.pacsai@gmail.com>
2019-01-17 11:09:21 +0100

    add instruction to completed exercises

--- hello-world.js
+++ hello-world.js
@@ -3,6 +3,19 @@
 // convenience to get you started writing code faster.
 //
 
+/*
+The classical introductory exercise. Just say 'Hello, World!'.
+
+'Hello, World!' is the traditional first program for beginning programming in a new language
+or environment.
+
+The objectives are simple:
+
+Write a function that returns the string 'Hello, World!'.
+Run the test suite and make sure that it succeeds.
+Submit your solution and check it at the website.
+*/
+
 export const hello = () => {
   //
   // YOUR CODE GOES HERE
