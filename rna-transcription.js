d5313a9dc169bbe5b4eb3d19df0b04a88267c765
unknown <judit.pacsai@gmail.com>
2019-01-19 00:00:03 +0100

    add solved exercise: simple-cipher

--- rna-transcription.js
+++ rna-transcription.js
@@ -17,21 +17,20 @@ A -> U
 */
 
 export const toRna = dna => dna.split('').map((n) => {
-    if (['G', 'C', 'T', 'A', ''].includes(n)) {
-      switch (n) {
-        case 'G':
-          return 'C';
-        case 'C':
-          return 'G';
-        case 'T':
-          return 'A';
-        case 'A':
-          return 'U';
-        default:
-          return '';
-      }
-    } else {
-      throw new Error('Invalid input DNA.');
+  if (['G', 'C', 'T', 'A', ''].includes(n)) {
+    switch (n) {
+      case 'G':
+        return 'C';
+      case 'C':
+        return 'G';
+      case 'T':
+        return 'A';
+      case 'A':
+        return 'U';
+      default:
+        return '';
     }
+  } else {
+    throw new Error('Invalid input DNA.');
+  }
 }).join('');
-  
