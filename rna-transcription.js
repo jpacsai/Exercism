d40e2d88545bf42028ee6ebd99705355243a6dfb
unknown <judit.pacsai@gmail.com>
2019-01-18 15:03:41 +0100

    refactor: throw error Object instead of String

--- rna-transcription.js
+++ rna-transcription.js
@@ -16,24 +16,24 @@ A -> U
 
 */
 
-export const toRna = dna => {
-    return dna.split('').map(n => {
-        if (['G', 'C', 'T', 'A', ''].includes(n)) {
-            switch(n) {
-                case 'G':
-                    return 'C';
-                case 'C':
-                    return 'G';
-                case 'T':
-                    return 'A';
-                case 'A':
-                    return 'U';
-                default:
-                    return '';
-            }
+export const toRna = (dna) => {
+    return dna.split('').map((n) => {
+      if (['G', 'C', 'T', 'A', ''].includes(n)) {
+        switch (n) {
+          case 'G':
+            return 'C';
+          case 'C':
+            return 'G';
+          case 'T':
+            return 'A';
+          case 'A':
+            return 'U';
+          default:
+            return '';
         }
-        else {
-            throw 'Invalid input DNA.';
-        }
+      } else {
+        throw new Error('Invalid input DNA.');
+      }
     }).join('');
-}
+  };
+  
